f0a2a006190af420e60c5b6bfa4738f9e1276316
KnechtRootrechtCH <github@atheos.cc>
2020-01-01 15:26:49 +0100

    #53: moved firebase config to separate file

--- src/config/fire.js
+++ src/config/fire.js
@@ -1,18 +1,10 @@
+import {fireConfig} from './fireConfig';
 import firebase from 'firebase/app';
 import auth from 'firebase/auth'; // eslint-disable-line no-unused-vars
 import store from 'firebase/firestore'; // eslint-disable-line no-unused-vars
 import func from 'firebase/functions'; // eslint-disable-line no-unused-vars
 
-const config = {
-    apiKey: 'AIzaSyD-HqBxheYcdQUAUB1beYZCX60U51G5L5A',
-    authDomain: 'varda-80e71.firebaseapp.com',
-    databaseURL: 'https://varda-80e71.firebaseio.com',
-    projectId: 'varda-80e71',
-    storageBucket: 'varda-80e71.appspot.com',
-    messagingSenderId: '629129663242'
-};
-
-const fire = firebase.initializeApp(config)
+const fire = firebase.initializeApp(fireConfig)
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
 const firestore = firebase.firestore();
 const functions = firebase.functions();
---
+++ src/config/fireConfig.js
@@ -0,0 +1,10 @@
+// check firebase console for correct values
+const fireConfig = {
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
+};
+export {fireConfig};
